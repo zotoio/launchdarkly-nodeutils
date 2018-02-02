e2fe527c8c093e44cb1c2a0327650a10462bd009
wyvern8 <wyvern@smartcreations.com.au>
2018-02-02 23:17:22 +1100

    fix(tidy): up

--- test/placeholder.spec.js
+++ test/placeholder.spec.js
@@ -1,7 +1,7 @@
 import { default as assert } from 'assert';
 import { describe, it } from 'mocha';
 
-describe('GithubTaskManager', function() {
+describe('LaunchDarklyUtils', function() {
     describe('setup', function() {
         it('should work', function(done) {
             assert.equal(1, 1);
